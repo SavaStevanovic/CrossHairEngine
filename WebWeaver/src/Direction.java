3250a7b905fb9a3547b3125465fbca45cb6d5d8d
SavaStevanovic <sava.stevanovic@pmf.edu.rs>
2017-06-04 17:58:16 +0200

    Server: Added missing file

--- WebWeaver/src/Direction.java
+++ WebWeaver/src/Direction.java
@@ -1,15 +1,17 @@
 
 public enum Direction {
-	UP("up", -1, 0), DOWN("down", 1, 0), LEFT("left", 0, -1), RIGHT("right", 0, 1), CENTER("center", 0, 0);
+	UP("up", -1, 0, 1), DOWN("down", 1, 0, 5), LEFT("left", 0, -1, 7), RIGHT("right", 0, 1, 3), CENTER("center", 0, 0, 0);
 
 	private final String name;
 	private final int xMove;
 	private final int yMove;
+	private final int directionBit;
 
-	private Direction(String name, int x, int y) {
+	private Direction(String name, int x, int y, int directionBit) {
 		this.name = name;
 		this.xMove = x;
 		this.yMove = y;
+		this.directionBit=directionBit;
 	}
 
 	public int getX() {
@@ -22,5 +24,9 @@ public enum Direction {
 
 	public String toString() {
 		return this.name;
+	}
+	
+	public int getDirectionBit() {
+		return this.directionBit;
 	}
 }
